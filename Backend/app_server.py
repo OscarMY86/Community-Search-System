7748ee7d20ef78610dc7bf4863d3c215be1e5362
OscarMY86 <oscarwong516@gmail.com>
2023-08-14 17:51:47 +0800

    send a get request to backend
    I have built a simple frontend which can send the name and k-value to backend. It works.

--- Backend/app_server.py
+++ Backend/app_server.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, jsonify
-import subprocess
+from flask_cors import CORS
+# import subprocess
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/search', methods=['POST'])
+@app.route('/')
+def index():
+    return "Testing server"
+
+@app.route('/search', methods=['GET'])
 def search():
-    data = request.get_json()
-    search_name = data['name']
-    k = data['k']
+    search_name = request.args.get('name')
+    k = request.args.get('k')
 
     def name_id(name):
-        with open('../DBLP-Geneartion/dataset/name2id.txt', 'r') as file:
+        with open('../DBLP-Geneartion/dataset/name2id.txt', 'r', encoding='utf-8') as file:
             for line in file:
                 parts = line.split()
                 if ' '.join(parts[:-1]) == name:
@@ -24,27 +29,33 @@ def search():
     with open("search.txt", "w") as f:
         f.write(str(id) + " " + search_name + " " + str(k) + "\n")
 
-    with open("../SurveyCS/SurveyCS/data/Astroph/core_querynodes.txt", "w") as f:
+
+    ###### Write the input id and k-value to 4 files ######
+    # with open("../SurveyCS/SurveyCS/data/Astroph/core_querynodes.txt", "w") as f:
+    #     f.write(str(id) + " " + str(k) + "\n")
+
+    with open("../SurveyCS/SurveyCS/data/Astroph/truss_querynodes.txt", "w", encoding='utf-8') as f:
         f.write(str(id) + " " + str(k) + "\n")
 
-    with open("../SurveyCS/SurveyCS/data/Astroph/truss_querynodes.txt", "w") as f:
-        f.write(str(id) + " " + str(k) + "\n")
+    # with open("../SurveyCS/SurveyCS/data/Astroph/query1.txt", "w") as f:
+    #     f.write(str(id) + " " + str(k) + "\n")
 
-    with open("../SurveyCS/SurveyCS/data/Astroph/query1.txt", "w") as f:
-        f.write(str(id) + " " + str(k) + "\n")
+    # with open("../SurveyCS/SurveyCS/data/Astroph/query_ecc.txt", "w") as f:
+    #     f.write(str(id) + " " + str(k) + "\n")
 
-    with open("../SurveyCS/SurveyCS/data/Astroph/query_ecc.txt", "w") as f:
-        f.write(str(id) + " " + str(k) + "\n")
 
-    command = ['../SurveyCS/SurveyCS/ktruss/online/ktcom', '../SurveyCS/SurveyCS/data/Astroph/']
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ###### Run the Algorithm ######
+    # command = ['../SurveyCS/SurveyCS/ktruss/online/ktcom', '../SurveyCS/SurveyCS/data/Astroph/']
+    # result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    if result.returncode == 0:
-        output = result.stdout.decode('utf-8')
-        return jsonify({'output': output})
-    else:
-        error = result.stderr.decode('utf-8')
-        return jsonify({'error': error}), 500
+    # if result.returncode == 0:
+    #     output = result.stdout.decode('utf-8')
+    #     return jsonify({'output': output})
+    # else:
+    #     error = result.stderr.decode('utf-8')
+    #     return jsonify({'error': error}), 500
+
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
--- comm-app/src/App.js
+++ comm-app/src/App.js
@@ -13,18 +13,40 @@ function App() {
     setk(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Construct the query string
+    const queryString = `name=${encodeURIComponent(name)}&k=${encodeURIComponent(k)}`;
+
+    // Make the GET request to the Flask backend
+    fetch(`http://localhost:5000/search?${queryString}`)
+      .then((response) => response.json())
+      .then((result) => {
+        // Handle the response from the backend
+        console.log(result);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error('Error:', error);
+      });
+  };
+  
   
   return (
     <div className="container">
       <h1>Social Network: Community Search Project</h1>
       <p> Please enter the search name and k value to search:</p>
 
-      <div className="input-container">
-        <label>Search name:</label>
-        <input type="text" value={name} onChange={handleNameChange} />
-        <label>K-value:</label>
-        <input type="text" value={k} onChange={handlekChange} />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Search name:</label>
+          <input type="text" value={name} onChange={handleNameChange} />
+          <label>K-value:</label>
+          <input type="text" value={k} onChange={handlekChange} />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
