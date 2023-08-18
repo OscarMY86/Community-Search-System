770a84a239e0a0c2a7ca304d76e05734341a031b
OscarMY86 <oscarwong516@gmail.com>
2023-08-18 16:03:54 +0800

    force direct graph

--- Backend/app_server.py
+++ Backend/app_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# import subprocess
+import json
+import subprocess
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +9,33 @@ CORS(app)
 @app.route('/')
 def index():
     return "Backend server is running"
+
+@app.route('/test', methods = {'GET'})
+def test():
+
+    ### Same from search.py ###
+    search_name = request.args.get('name')
+    k = request.args.get('k')
+    method = request.args.get('option')
+
+    def name_id(name):
+        with open('../DBLP-Geneartion/dataset/name2id.txt', 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.split()
+                if ' '.join(parts[:-1]) == name:
+                    return int(parts[-1])
+        return -1
+
+    id = name_id(search_name)
+    if id == -1:
+        return None
+
+    with open("search.txt", "w") as f:
+        f.write("Id: " + str(id) + "\n" + "Name: " + search_name + "\n" + "k-value: " + str(k) + "\n" + "Method: " + method )
+
+    with open('./testset/test1.json') as json_file:
+        data = json.load(json_file)
+    return jsonify(data)
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -25,29 +53,35 @@ def search():
 
     id = name_id(search_name)
     if id == -1:
-        return jsonify({'error': 'Unfound name'})
+        return None
 
     with open("search.txt", "w") as f:
         f.write("Id: " + str(id) + "\n" + "Name: " + search_name + "\n" + "k-value: " + str(k) + "\n" + "Method: " + method )
 
-
     ###### Write the input id and k-value to 4 files ######
-    # with open("../SurveyCS/SurveyCS/data/Astroph/core_querynodes.txt", "w") as f:
-    #     f.write(str(id) + " " + str(k) + "\n")
+    if (method == "core"):
+        with open("../SurveyCS/SurveyCS/data/Astroph/core_querynodes.txt", "w") as f:
+            f.write(str(id) + " " + str(k) + "\n")
+        command = ['../SurveyCS/SurveyCS/ktruss/online/kccom', '../SurveyCS/SurveyCS/data/Astroph/']
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    if (method == "truss"):
+        with open("../SurveyCS/SurveyCS/data/Astroph/truss_querynodes.txt", "w", encoding='utf-8') as f:
+            f.write(str(id) + " " + str(k) + "\n")
+        command = ['../SurveyCS/SurveyCS/ktruss/online/ktcom', '../SurveyCS/SurveyCS/data/Astroph/']
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    with open("../SurveyCS/SurveyCS/data/Astroph/truss_querynodes.txt", "w", encoding='utf-8') as f:
-        f.write(str(id) + " " + str(k) + "\n")
+    # if (method == "clique"):
+    #     with open("../SurveyCS/SurveyCS/data/Astroph/query1.txt", "w") as f:
+    #         f.write(str(id) + " " + str(k) + "\n")
 
-    # with open("../SurveyCS/SurveyCS/data/Astroph/query1.txt", "w") as f:
-    #     f.write(str(id) + " " + str(k) + "\n")
-
-    # with open("../SurveyCS/SurveyCS/data/Astroph/query_ecc.txt", "w") as f:
-    #     f.write(str(id) + " " + str(k) + "\n")
-
+    # if (method == "ecc"):
+    #     with open("../SurveyCS/SurveyCS/data/Astroph/query_ecc.txt", "w") as f:
+    #         f.write(str(id) + " " + str(k) + "\n")
 
     ###### Run the Algorithm ######
-    # command = ['../SurveyCS/SurveyCS/ktruss/online/ktcom', '../SurveyCS/SurveyCS/data/Astroph/']
-    # result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = ['../SurveyCS/SurveyCS/ktruss/online/ktcom', '../SurveyCS/SurveyCS/data/Astroph/']
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # if result.returncode == 0:
     #     output = result.stdout.decode('utf-8')
--- comm-app/src/App.js
+++ comm-app/src/App.js
@@ -1,65 +1,93 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState} from 'react';
+import ForceDirectedGraph from './ForceDirectedGraph.js';
 
 function App() {
   const [name, setName] = useState('');
   const [k, setk] = useState('');
   const [selectedOption, setSelectedOption] = useState('');
+  const [graphData, setGraphData] = useState('');
+  const [error, setError] = useState(null);
 
   const handleNameChange = (event) => {
     setName(event.target.value);
   };
 
   const handlekChange = (event) => {
-    setk(event.target.value);
+    const value = event.target.value;
+    if (/^\d*$/.test(value)) {
+      setk(value);
+    }
   };
   
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
-
+  
   const handleSubmit = (event) => {
     event.preventDefault();
 
     // Construct the query string
     const queryString = `name=${encodeURIComponent(name)}&k=${encodeURIComponent(k)}&option=${encodeURIComponent(selectedOption)}`;
-    // const queryString = `name=${encodeURIComponent(name)}&k=${encodeURIComponent(k)}`;
+
     // Make the GET request to the Flask backend
-    fetch(`http://localhost:5000/search?${queryString}`)
+    // fetch(`http://localhost:5000/search?${queryString}`) // Search API
+    fetch(`http://localhost:5000/test?${queryString}`) // Test API
       .then((response) => response.json())
       .then((result) => {
         // Handle the response from the backend
-        console.log(result);
+        if (result == null){
+          setGraphData(null);
+          setError('Invalid input');
+        } else {
+          setGraphData(result);
+          setError(null);
+          console.log(result);
+        }
       })
       .catch((error) => {
-        // Handle any errors
         console.error('Error:', error);
+        setError('Make sure you input correctly.');
+        setGraphData(null);
       });
   };
 
+  const handleReset = () => {
+    setName('');
+    setk('');
+    setSelectedOption('');
+  };
   
   return (
     <div className="container">
       <h1>Social Network: Community Search Project</h1>
       <p> Please enter the search name and k value to search:</p>
-
       <form onSubmit={handleSubmit}>
         <dic className="input-container">
           <label>Search name:</label>
-          <input type="text" value={name} onChange={handleNameChange} />
+          <input type="text" value={name} onChange={handleNameChange} placeholder="Input search name" />
         </dic>
         <dic className="input-container">
           <label>K-value:</label>
-          <input type="text" value={k} onChange={handlekChange} />
+          <input type="integer" value={k} onChange={handlekChange} placeholder="Input integer"/> 
           <select value={selectedOption} onChange={handleOptionChange}>
             <option value="">Please select Method</option>
             <option value="core">Core</option>
             <option value="truss">Truss</option>
             <option value="clique">Clique</option>
+            <option value="ecc">Ecc</option>
           </select>
         </dic>
-        <button type="submit">Submit</button>
+        <button onClick={(event) => handleSubmit(event)}>Submit</button>
+        <button type='button' onClick={handleReset}>Reset</button>
       </form>
+      {error && <div className="error">{error}</div>}
+      <div>
+        {graphData !== null ? ( <ForceDirectedGraph graphData={graphData} />
+        ) : (
+          <div>No graph data available.</div>
+        )}
+      </div>
     </div>
   );
 }
