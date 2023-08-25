127659e6db16bcf49440b4472045ed43c43e1075
OscarMY86 <oscarwong516@gmail.com>
2023-08-25 16:39:08 +0800

    Add docker

--- Backend/app_server.py
+++ Backend/app_server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 import subprocess
+import converter
 
 app = Flask(__name__)
 CORS(app)
@@ -33,18 +34,23 @@ def test():
     with open("search.txt", "w") as f:
         f.write("Id: " + str(id) + "\n" + "Name: " + search_name + "\n" + "k-value: " + str(k) + "\n" + "Method: " + method )
 
-    with open('./testset/test3.json') as json_file:
-        data = json.load(json_file)
-    return jsonify(data)
+    txt_file_path = f'./SurveyCS/SurveyCS/data/Astroph/trusscom/{id}.txt'
+    json_data = converter.convert_to_json(txt_file_path)
+
+    # with open('./testset/test3.json') as json_file:
+    #     data = json.load(json_file)
+    return jsonify(json_data)
 
 @app.route('/search', methods=['GET'])
 def search():
     search_name = request.args.get('name')
     k = request.args.get('k')
     method = request.args.get('option')
+    txt_file_path = ''
+    # json_file_path = './testset/result.json'
 
     def name_id(name):
-        with open('../DBLP-Geneartion/dataset/name2id.txt', 'r', encoding='utf-8') as file:
+        with open('name2id.txt', 'r', encoding='utf-8') as file:
             for line in file:
                 parts = line.split()
                 if ' '.join(parts[:-1]) == name:
@@ -59,38 +65,27 @@ def search():
         f.write("Id: " + str(id) + "\n" + "Name: " + search_name + "\n" + "k-value: " + str(k) + "\n" + "Method: " + method )
 
     ###### Write the input id and k-value to 4 files ######
+
+    if (method == "clique" or method == "ecc"):
+        return None
+
     if (method == "core"):
-        with open("../SurveyCS/SurveyCS/data/Astroph/core_querynodes.txt", "w") as f:
+        with open("./SurveyCS/SurveyCS/data/Astroph/core_querynodes.txt", "w") as f:
             f.write(str(id) + " " + str(k) + "\n")
-        command = ['../SurveyCS/SurveyCS/ktruss/online/kccom', '../SurveyCS/SurveyCS/data/Astroph/']
+        command = ['./SurveyCS/SurveyCS/kcore/online/kccom', './SurveyCS/SurveyCS/data/Astroph/']
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
+        txt_file_path = f'./SurveyCS/SurveyCS/data/Astroph/corecom/{id}.txt'
+        json_data = converter.convert_to_json(txt_file_path)
+
     if (method == "truss"):
-        with open("../SurveyCS/SurveyCS/data/Astroph/truss_querynodes.txt", "w", encoding='utf-8') as f:
+        with open("./SurveyCS/SurveyCS/data/Astroph/truss_querynodes.txt", "w", encoding='utf-8') as f:
             f.write(str(id) + " " + str(k) + "\n")
-        command = ['../SurveyCS/SurveyCS/ktruss/online/ktcom', '../SurveyCS/SurveyCS/data/Astroph/']
+        command = ['./SurveyCS/SurveyCS/ktruss/online/ktcom', './SurveyCS/SurveyCS/data/Astroph/']
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        txt_file_path = f'./SurveyCS/SurveyCS/data/Astroph/trusscom/{id}.txt'
+        json_data = converter.convert_to_json(txt_file_path)
 
-    # if (method == "clique"):
-    #     with open("../SurveyCS/SurveyCS/data/Astroph/query1.txt", "w") as f:
-    #         f.write(str(id) + " " + str(k) + "\n")
-
-    # if (method == "ecc"):
-    #     with open("../SurveyCS/SurveyCS/data/Astroph/query_ecc.txt", "w") as f:
-    #         f.write(str(id) + " " + str(k) + "\n")
-
-    ###### Run the Algorithm ######
-    # command = ['../SurveyCS/SurveyCS/ktruss/online/ktcom', '../SurveyCS/SurveyCS/data/Astroph/']
-    # result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    # if result.returncode == 0:
-    #     output = result.stdout.decode('utf-8')
-    #     return jsonify({'output': output})
-    # else:
-    #     error = result.stderr.decode('utf-8')
-    #     return jsonify({'error': error}), 500
-
-    return jsonify({'status': 'success'})
+    return jsonify(json_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0")
--- Backend/converter.py
+++ Backend/converter.py
@@ -1,26 +1,40 @@
 import json
 
-txt_file_path = './SurveyCS/SurveyCS/data\Astroph/trusscom/1077429.txt'
-json_file_path = './testset/test2.json'
+def convert_to_json(txt_file_path):
+    with open(txt_file_path, 'r') as file:
+        txt_data = file.read()
 
-with open(txt_file_path, 'r') as file:
-    txt_data = file.read()
+    lines = txt_data.split('\n')
+    data = []
 
-lines = txt_data.split('\n')
-data = []
+    # Process each line and convert it to JSON format
+    for line in lines:
+        pair = line.split()
+        if len(pair) == 2:
+            data.append({
+                'source': pair[0],
+                'target': pair[1]
+            })
 
-# Process each line and convert it to JSON format
-for line in lines:
-    pair = line.split()
-    if len(pair) == 2:
-        data.append({
-            'source': pair[0],
-            'target': pair[1]
-        })
+    # Convert the data to JSON
+    json_data = json.dumps(data)
 
-# Convert the data to JSON
-json_data = json.dumps(data)
+    return json_data
 
-# Save the JSON data to a file
-with open(json_file_path, 'w') as file:
-    file.write(json_data)
+# with open('./SurveyCS/SurveyCS/data/Astroph/trusscom/495623.txt', 'r') as file:
+#     lines = file.readlines()
+
+# data = []
+
+# # Process each line and create a list of dictionaries
+# for line in lines:
+#     values = line.split()
+#     if len(values) == 2:
+#         data.append({
+#             'source': int(values[0]),
+#             'target': int(values[1])
+#         })
+
+# # Write the data to a .json file
+# with open('./testset/result.json', 'w') as file:
+#     json.dump(data, file)
